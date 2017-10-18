f488aac06459ca62ff9ece5440f4c9f6f80f9b87
lop3ziv4n <lopez_ivan_leandro@yahoo.com.ar>
2017-10-18 10:13:29 -0300

    Update app.modules.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {AngularFireModule} from 'angularfire2';
@@ -8,11 +8,11 @@ import {AngularFireDatabaseModule} from 'angularfire2/database';
 import {AngularFireAuthModule} from 'angularfire2/auth';
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBCfFLnNuwh9Lh7cORu4BjBcm5SQvKOBP4",
-  authDomain: "angular-firebase-note.firebaseapp.com",
-  databaseURL: "https://angular-firebase-note.firebaseio.com",
-  storageBucket: "angular-firebase-note.appspot.com",
-  messagingSenderId: '1073562697896'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  storageBucket: '',
+  messagingSenderId: ''
 };
 
 @NgModule({
